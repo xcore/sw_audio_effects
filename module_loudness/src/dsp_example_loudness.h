b730a6478186e6689c80663d55971d875478c044
beaumontm <markb@xmos.com>
2012-09-27 16:21:55 +0100

    Added xpd build_results

---
+++ module_loudness/src/dsp_example_loudness.h
@@ -0,0 +1,39 @@
+/******************************************************************************\
+ * Header:  dsp_loudness
+ * File:    dsp_loudness.h
+ * Author: Mark Beaumont
+ * Description: Definitions, types, and prototypes for dsp_loudness.xc
+ *
+ * Version: 0v1
+ * Build:
+ *
+ * The copyrights, all other intellectual and industrial
+ * property rights are retained by XMOS and/or its licensors.
+ * Terms and conditions covering the use of this code can
+ * be found in the Xmos End User License Agreement.
+ *
+ * Copyright XMOS Ltd 2012
+ *
+ * In the case where this code is a modification of existing code
+ * under a separate license, the separate license terms are shown
+ * below. The modifications to the code are still covered by the
+ * copyright notice above.
+ *
+\******************************************************************************/
+
+#ifndef _DSP_LOUDNESS_H_
+#define _DSP_LOUDNESS_H_
+
+#include "types64bit.h"
+#include "common_utils.h"
+#include "non_linear_gain.h"
+
+/******************************************************************************/
+void dsp_loudness( // Thread that applies non-linear gain control to stream of audio samples
+	streaming chanend c_dsp_gain // Channel connecting to DSP-control thread (bi-directional)
+);
+/******************************************************************************/
+
+#endif // _DSP_LOUDNESS_H_
+/******************************************************************************/
+// dsp_loudness.h
