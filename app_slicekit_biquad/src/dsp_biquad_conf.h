daac586db9b5828331d1f3dfb973abc41cba53c3
David Lacey <david.lacey@xmos.com>
2012-10-10 13:44:38 +0100

    Added missing files

---
+++ app_slicekit_biquad/src/dsp_biquad_conf.h
@@ -0,0 +1,6 @@
+#ifndef __dsp_biquad_h__
+#define __dsp_biquad_h__
+
+#define NUM_BIQUAD_CHANS 4
+
+#endif
---
+++ module_dsp_biquad/dsp_biquad_conf_example.h
@@ -0,0 +1,7 @@
+#ifndef __dsp_biquad_h__
+#define __dsp_biquad_h__
+
+// Set this to control how manner channels the biquad functions expect
+#define NUM_BIQUAD_CHANS 4
+
+#endif
